a02c2f7b6c68ee51067254b8f5807ebc5f7268c2
Jaewon Moon <jmatt@naver.com>
2023-12-02 23:44:03 -0500

    serializer and selenium are added

--- ClassWise/models.py
+++ ClassWise/models.py
@@ -4,12 +4,16 @@ from django.db import models
 class Course(models.Model):
     course_name = models.CharField(max_length=200, null=True)
     course_code = models.CharField(max_length=200, null=True)
-    course_duration = models.CharField(max_length=200, null=True)
-    course_fee = models.CharField(max_length=200, null=True)
+    # TODO: indicate which instructor is for which semester.
+    course_instructors = models.CharField(max_length=200, null=True)
+    # could be multiple
+    course_prerequisites = models.CharField(max_length=200, null=True)
+    # could be multiple
+    course_offering_terms = models.CharField(max_length=200, null=True)
+    # multiple
+    course_previous_grade = models.CharField(max_length=200, null=True)
     course_description = models.CharField(max_length=200, null=True)
-    course_image = models.ImageField(null=True, blank=True)
-    course_date_created = models.DateTimeField(auto_now_add=True, null=True)
-    # TODO
+    #course_credit = models.IntegerField(null=False)
     comments = models.TextField(null=True, blank=True)
     def __str__(self):
         return self.course_name
--- ClassWise/urls.py
+++ ClassWise/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('registration/', views.registrationPage, name="registration"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('', views.home, name="home"),
+    path('courses/', views.course_list, name="course-list"),
+    path('courses/<int:pk>/', views.course_detail, name="course-detail"),
+    #path('users/', views.user_list, name="user-list"),
+    #path('users/<int:pk>/', views.user_detail, name="user-detail"),
 ]
+
+# We don't necessarily need to add these extra url patterns in, but it gives us a simple,
+# clean way of referring to a specific format.
+urlpatterns = format_suffix_patterns(urlpatterns)
