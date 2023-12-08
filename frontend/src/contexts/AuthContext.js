7e0b36141ec1b3bf12dcf098b5fdff500249aff2
Daniel Zhang <danzhang2002@gmail.com>
2023-12-08 18:30:04 -0500

    sync

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,14 +1,16 @@
 import { createContext, useState, useEffect } from "react";
-import jwt_decode from "jwt-decode";
-import { useHistory } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-	const [user, setUser] = useState("");
+	const navigate = useNavigate();
 
+	const [user, setUser] = useState("");
+	const [authTokens, setAuthTokens] = useState("");
 	// const [user1, setUser1] = useState(() =>
 	// 	localStorage.getItem("authTokens")
 	// 		? jwt_decode(localStorage.getItem("authTokens"))
@@ -20,70 +22,37 @@ export const AuthProvider = ({ children }) => {
 	// 		: null
 	// );
 
-	// const history = useHistory();
+	const loginUser = async (e) => {
+		e.preventDefault();
+		// TODO fix url
+		const response = await fetch("http://localhost:5000/auth/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: e.target.username.value,
+				password: e.target.password.value,
+			}),
+		});
+		const data = await response.json();
 
-	// const loginUser = async (e) => {
-	// 	e.preventDefault();
-	// 	// TODO fix url
-	// 	const response = await fetch("http://localhost:5000/api/auth/login", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json",
-	// 		},
-	// 		body: JSON.stringify({
-	// 			username: e.target.username.value,
-	// 			password: e.target.password.value,
-	// 		}),
-	// 	});
-	// 	const data = await response.json();
+		if (response.status === 200) {
+			setAuthTokens(data);
+			setUser(jwtDecode(data.access));
+			localStorage.setItem("authTokens", JSON.stringify(data));
+			navigate("/search");
+		} else {
+			alert("something wrong");
+		}
+	};
 
-	// 	if (response.status === 200) {
-	// 		setAuthTokens(data);
-	// 		setUser(jwt_decode(data.access));
-	// 		localStorage.setItem("authTokens", JSON.stringify(data));
-	// 		history.push("/search");
-	// 	} else {
-	// 		alert("something wrong");
-	// 	}
-	// };
-
-	// const logout = () => {
-	// 	setAuthTokens(null);
-	// 	setUser(null);
-	// 	localStorage.removeItem("authTokens");
-	// 	history.push("/login");
-	// };
-
-	// const updateToken = async () => {
-	// 	let response = await fetch("http://localhost:5000/api/auth/refresh", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json",
-	// 		},
-	// 		body: JSON.stringify({
-	// 			refresh: authTokens.refresh,
-	// 		}),
-	// 	});
-
-	// 	if (response.status === 200) {
-	// 		const data = await response.json();
-	// 		setAuthTokens(data);
-	// 		setUser(jwt_decode(data.access));
-	// 		localStorage.setItem("authTokens", JSON.stringify(data));
-	// 	} else {
-	// 		logout();
-	// 	}
-	// };
-
-	// useEffect(() => {
-	// 	if (authTokens) {
-	// 		const decoded = jwt_decode(authTokens.access);
-	// 		const currentTime = Date.now() / 1000;
-	// 		if (decoded.exp < currentTime) {
-	// 			updateToken();
-	// 		}
-	// 	}
-	// }, [authTokens]);
+	const logout = () => {
+		setAuthTokens(null);
+		setUser(null);
+		localStorage.removeItem("authTokens");
+		navigate("/login");
+	};
 
 	const contextData = {
 		user: user,
